a4c098cd9e8a83bbea46b16985272873e50c20d7
Keron Matthews <kdmatth@outlook.com>
2025-11-12 21:09:16 -0400

    Update App.tsx
    Added routes to Contact page, RequestDemo Page and made the landingPage the root

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import TicketNew from "./pages/TicketNew";
 import Chat from "./pages/Chat";
+import LandingPage from "./pages/LandingPage";
+import RequestDemo from "./pages/RequestDemo";
+import ContactUs from "./pages/ContactUs";
 import ProtectedRoute from "./lib/ProtectedRoute";
 import { AuthProvider } from "./lib/auth"; // 👈 Wrap your app with global auth
 import "./App.css";
@@ -16,13 +19,15 @@ export default function App() {
         <div style={{ padding: 16 }}>
           <Routes>
             {/* Redirect root to login */}
-            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/" element={<Navigate to="/LandingPage" replace />} />
 
             {/* Public routes */}
+            <Route path="/LandingPage" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/tickets/new" element={<TicketNew />} />
             <Route path="/chat" element={<Chat />} />
-
+            <Route path="/demo" element={<RequestDemo />} />
+            <Route path="/contact" element={<ContactUs />} />
             {/* Protected route (only employees can access Dashboard) */}
             <Route
               path="/dashboard"
